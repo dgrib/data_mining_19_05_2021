2230e73de179864eb59ef893327af9951bb36e76
dgrib <gds194@bk.ru>
2021-06-05 20:20:44 +0500

    04 HW cleaning before HW

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# через него будем запускать и отлаживаться
-# в рамках одного проекта может рабогтать несколько пауков, ктото должен ими управлять,
-# для этого есть класс управляющегно процесса, через него запускаются все остальные пауки
-from scrapy.crawler import CrawlerProcess  # мы только запускаем процессс а он сам рулит пауками запросами настройками
-from scrapy.settings import Settings  # чтобы корректно импортровать все настройки
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
 
-from gb_parse.spiders.autoyoula import AutoyoulaSpider  # импортируем нашего паука
+from gb_parse.spiders.autoyoula import AutoyoulaSpider
 
 if __name__ == '__main__':
-    crawler_settings = Settings()  # инициализируем класс Settings
-    crawler_settings.setmodule("gb_parse.settings")  # наш экз класса settings считает файл и правильно настроится
-    crawler_process = CrawlerProcess(settings=crawler_settings)  # создаем процесс,
-    # теперь процесс подготовлен чтобы начать работу, но он ничего не знает еще о наших пауках
-    crawler_process.crawl(AutoyoulaSpider)  # добавляем паука в процесс, добавляем класс!!! не экземпляр
-    # crawler_process.crawl(AutoyoulaSpider) # может быть несколько разнеых паукаов, а старт один,
-    # один же процесс для всех пауков. Сам экз класса будет создавать наш процесс
+    crawler_settings = Settings()
+    crawler_settings.setmodule("gb_parse.settings")
+    crawler_process = CrawlerProcess(settings=crawler_settings)
+    crawler_process.crawl(AutoyoulaSpider)
     crawler_process.start()
